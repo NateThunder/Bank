3c1cd1d337f1977edf80a8b029f3f2c561bf1604
nateThunder <n.somevi@hotmail.com>
2025-03-12 19:12:46 +0000

    Add error checking for existing account

--- Bank.py
+++ Bank.py
@@ -1,13 +1,13 @@
 import os
-# Welcome message
+#-------------------------------- Welcome message --------------------------------------------------------
 print("Welcome to Dope A F bank. The home of your banking needs ")
 
-######### Log into your bank########################
+#------------------------------ Log into your bank --------------------------------------------------------
 
 
 
 
-# Do you have an account?
+#-------------------------- Do you have an account? -------------------------------------------------------
 def main():
     start = input("Do you have an account. Please type Y for yes and N for No. ").capitalize().strip()
     os.system("cls")
@@ -21,34 +21,39 @@ def main():
             customer_file(inname, inpin)
             start="Y"
 
-    # login       
+#-------------------------------------- login -----------------------------------------------------------------
         n =3
         while n >0:
             
             if start == "Y":
-                while n == 3:
-                    print("OK lets Log into your Dope A F bank account \n")
-                    break
-                name = input("Please enter your user name ")
-                pin = int(input("Please enter your pin "))
-                os.system("cls")
-            if pin == inpin and name ==inname:
-                print(f"Welcome to your Dope A F account {name.capitalize()}")
-                first=input("Would you like to do. Enter C for check balance, D to deposit, W to withdraw and H for transaction history, ").capitalize()
-                if first == "C":
-                    account(name)
-                break  
+                try: # Does the account excist
+                    while n == 3:
+                        print("OK lets Log into your Dope A F bank account \n")
+                        break
+                    name = input("Please enter your user name ")
+                    pin = int(input("Please enter your pin "))
+                    os.system("cls")
+#------------------ Change to read files -----------------------------------------------------------------
+                    if pin == inpin and name ==inname:
+                        print(f"Welcome to your Dope A F account {name.capitalize()}")
+                        first=input("Would you like to do. Enter C for check balance, D to deposit, W to withdraw and H for transaction history, ").capitalize()
+                        if first == "C":
+                            account(name)
+                        break  
+                except: # Start again
+                    print("The account is not recognised. Try registering for an account")
+                    main()
             
             else:
-                n-=1
+                n-=1  # Atempts
                 print(f"wrong credential {n} more atempts left.")
                
-        ######write information to a text file####
+#------------------- Write information to a text file ------------------------------------------------------
         customer_file(name, pin)
 
         break
     
-#-------------------Bank acount information -------------------------------------------------
+#------------------- Bank acount information -------------------------------------------------
 def account(name):
    
     print("welcome to your account you have £100")
@@ -58,11 +63,11 @@ def account(name):
     print(blank)
     option = input("Type W if you want to withdraw money or type D to deposite money").lower() #withdraw or deposit
     
-    ################withdrawal#################################################
+#---------------------------- Withdrawal ----------------------------------------------------------
     if option == "w":
         figure = float(input("how much would you like to withdraw?"))
 
-#-------------------Text file created for new customer---------------------------------------------
+#------------------- Text file created for new customer ---------------------------------------------
 def customer_file(inname, inpin):
     file_name= f"{inname}.txt"
     open(file_name, "w", encoding="utf-8").write(f"Name = {inname}\nPin = {inpin}\nOverdraft = X\nTotal = Y\nTransactions = Z")
