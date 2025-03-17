52c19adefcf8338864027a29ef16007626ad1d9b
nateThunder <n.somevi@hotmail.com>
2025-03-17 23:15:32 +0000

    Input and ouptput money ha 2 decimal places

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 import os
 import time
-name = "nate"
-blank = open(f"{name}.txt","r", encoding="utf-8").read().split()
-overdraft = input("What would you like your overdraft to be?: £")
-blank[8]=str(overdraft)
-update = " ".join(blank)
-open(f"{name}.txt","w", encoding="utf-8").write(update)
-os.system("cls")
-print(f"Your overdraft is now {blank[8]}, that's Dope A F. Remember it is not free money. Enjoy!")
-print(blank)
+
+rep  =  open("bank_logo.txt", "r", encoding="utf-8").read()
+new = rep.replace("$", "£")
+open("pound_logo.txt","w",encoding="utf-8").write(new)
+print(new)
